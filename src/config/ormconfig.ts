7e77ccc23a46177538b2573bcd17e3b5202ad6b2
gideaopinheiro <gpss@ic.ufal.br>
2024-09-19 18:18:19 -0300

    chore: change database envs configuration

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -6,11 +6,7 @@ dotenvConfig({ path: '.env' });
 
 const config = {
   type: 'postgres',
-  host: `${process.env.DB_HOST}`,
-  port: Number(process.env.DB_PORT),
-  username: `${process.env.DB_USER}`,
-  password: `${process.env.DB_PASS}`,
-  database: `${process.env.DB_NAME}`,
+  url: `${process.env.DATABASE_URL}`,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/../migrations/*{.ts,.js}'],
   autoLoadEntities: true,
